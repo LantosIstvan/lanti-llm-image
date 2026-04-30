38cfdd591ec16f08f14d8f1191f18cae8e993f8a
István Lantos <lantosistvan89@gmail.com>
2026-04-30 15:09:41 +0200

    TÖRÖLVE: Beépített ComfyUI Manager használata

--- src/comfyui/docker-entrypoint.sh
+++ src/comfyui/docker-entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -e
 
+echo "Fixing permissions for mapped volumes..."
+# A felcsatolt mappákat a Docker root-ként hozza létre, jogosultságot kell adni a comfyuser-nek
+sudo chown -R comfyuser:comfyuser /usr/src/ComfyUI/user 2>/dev/null || true
+sudo chown -R comfyuser:comfyuser /usr/src/ComfyUI/output 2>/dev/null || true
+sudo chown -R comfyuser:comfyuser /usr/src/ComfyUI/models/SEEDVR2 2>/dev/null || true
+
 # Run command with python3 if the first argument contains a "-" or is not a system command. The last
 # part inside the "{}" is a workaround for the following bug in ash/dash:
 # https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=874264
