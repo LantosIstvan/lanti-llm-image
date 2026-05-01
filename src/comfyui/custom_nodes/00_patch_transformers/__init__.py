057d1b3db3accd5b17d30cc66a2c053890e21870
István Lantos <lantosistvan89@gmail.com>
2026-05-01 17:50:11 +0200

    torch.compile engedélyezése

--- src/comfyui/custom_nodes/00_patch_transformers/__init__.py
+++
@@ -1,9 +0,0 @@
-# Patch for transformers>=5.5.0 bug: 'flash_attn' missing from PACKAGE_DISTRIBUTION_MAPPING
-try:
-    from transformers.utils.import_utils import PACKAGE_DISTRIBUTION_MAPPING
-    if "flash_attn" not in PACKAGE_DISTRIBUTION_MAPPING:
-        PACKAGE_DISTRIBUTION_MAPPING["flash_attn"] = ["flash_attn", "flash-attn"]
-except Exception:
-    pass
-
-NODE_CLASS_MAPPINGS = {}
