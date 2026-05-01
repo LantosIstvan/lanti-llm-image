614ddddc978533d28cf327604ca72505bdf75378
István Lantos <lantosistvan89@gmail.com>
2026-05-01 16:44:52 +0200

    torch.compile engedélyezése

--- src/comfyui/docker-entrypoint.sh
+++ src/comfyui/docker-entrypoint.sh
@@ -5,7 +5,10 @@ echo "Fixing permissions for mapped volumes..."
 # A felcsatolt mappákat a Docker root-ként hozza létre, jogosultságot kell adni a comfyuser-nek
 sudo chown -R comfyuser:comfyuser /usr/src/ComfyUI/user 2>/dev/null || true
 sudo chown -R comfyuser:comfyuser /usr/src/ComfyUI/output 2>/dev/null || true
-# sudo chown -R comfyuser:comfyuser /usr/src/ComfyUI/models/SEEDVR2 2>/dev/null || true
+
+# TORCH.COMPILE CACHE JOGOSULTSÁG
+mkdir -p /usr/src/ComfyUI/user/triton_cache
+sudo chown -R comfyuser:comfyuser /usr/src/ComfyUI/user/triton_cache 2>/dev/null || true
 
 echo "Configuring ComfyUI-Manager security settings..."
 MANAGER_DIR="/usr/src/ComfyUI/user/__manager"
@@ -36,21 +39,21 @@ else
     fi
 fi
 
-echo "Applying patch for transformers>=5.5.0 flash_attn bug..."
-PATCH_DIR="/usr/src/ComfyUI/custom_nodes/00_patch_transformers"
-mkdir -p "$PATCH_DIR"
-cat << 'EOF' > "$PATCH_DIR/__init__.py"
-# Patch for transformers>=5.5.0 bug: 'flash_attn' missing from PACKAGE_DISTRIBUTION_MAPPING
-try:
-    from transformers.utils.import_utils import PACKAGE_DISTRIBUTION_MAPPING
-    if "flash_attn" not in PACKAGE_DISTRIBUTION_MAPPING:
-        PACKAGE_DISTRIBUTION_MAPPING["flash_attn"] =["flash_attn", "flash-attn"]
-except Exception:
-    pass
+# echo "Applying patch for transformers>=5.5.0 flash_attn bug..."
+# PATCH_DIR="/usr/src/ComfyUI/custom_nodes/00_patch_transformers"
+# mkdir -p "$PATCH_DIR"
+# cat << 'EOF' > "$PATCH_DIR/__init__.py"
+# # Patch for transformers>=5.5.0 bug: 'flash_attn' missing from PACKAGE_DISTRIBUTION_MAPPING
+# try:
+#     from transformers.utils.import_utils import PACKAGE_DISTRIBUTION_MAPPING
+#     if "flash_attn" not in PACKAGE_DISTRIBUTION_MAPPING:
+#         PACKAGE_DISTRIBUTION_MAPPING["flash_attn"] =["flash_attn", "flash-attn"]
+# except Exception:
+#     pass
 
-NODE_CLASS_MAPPINGS = {}
-EOF
-sudo chown -R comfyuser:comfyuser "$PATCH_DIR" 2>/dev/null || true
+# NODE_CLASS_MAPPINGS = {}
+# EOF
+# sudo chown -R comfyuser:comfyuser "$PATCH_DIR" 2>/dev/null || true
 
 # Run command with python3 if the first argument contains a "-" or is not a system command. The last
 # part inside the "{}" is a workaround for the following bug in ash/dash:
